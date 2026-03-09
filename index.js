7ec047e8a9fd3693a7c03078fa80355bb5dd3541
Ilayaraju2003 <ilayaraju2003@gmail.com>
2026-03-09 15:22:03 +0530

    first commit

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ app.use(express.json());
 app.use("/api/profile", profileRoutes);
 
 app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+  console.log("Server running on port${PORT}");
 });
